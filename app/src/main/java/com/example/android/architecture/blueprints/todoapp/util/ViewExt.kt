8101d41e408b15d26ee92a7e238ccd1fd3e83782
Aza Tulepbergenov <atulep@google.com>
2021-07-14 11:39:15 -0700

    Removes tests and modernizes build.
    Change-Id: I353c7182ddc2e85427171551a020be300072725d

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/util/ViewExt.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/util/ViewExt.kt
@@ -35,15 +35,6 @@ import com.google.android.material.snackbar.Snackbar
  */
 fun View.showSnackbar(snackbarText: String, timeLength: Int) {
     Snackbar.make(this, snackbarText, timeLength).run {
-        addCallback(object : Snackbar.Callback() {
-            override fun onShown(sb: Snackbar?) {
-                EspressoIdlingResource.increment()
-            }
-
-            override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
-                EspressoIdlingResource.decrement()
-            }
-        })
         show()
     }
 }
